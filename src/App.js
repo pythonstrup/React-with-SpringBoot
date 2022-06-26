f80f275b19bd0ac538b5c668e729608bee6dc196
BigBell <pythonstrup@gmail.com>
2022-06-26 17:03:38 +0900

    React Hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { createRef, useRef, useState } from "react";
 
-let a = 10;  // 변수
-const b= 20; // 상수
 
 function App() {
-  let list = [1, 2, 3];
+  
+  const myRef = useRef(null);
+
+  const [list, setList] = useState([
+    {id:1, name:"Hong"},
+    {id:2, name:"Park"},
+  ])
+
+  // 여러 개를 처리할 때는 배열로 만들어야 한다.
+  // createRef: Ref를 동적으로 생성해준다.
+  const myRefs = Array.from({length: list.length}).map(() => createRef());
 
   return (
     <div>
-      <div>
-        {list.map((n)=><h1>{n}</h1>)}
-      </div>
+      <button onClick={() => {
+        console.log(myRef);
+        myRef.current.style.backgroundColor='red';
+
+        myRefs[0].current.style.backgroundColor='green';
+        myRefs[1].current.style.backgroundColor='blue';
+        }}>
+          색 변경
+      </button>
+      <div ref={myRef}>박스</div>
+
+      {list.map((user, index) => (
+        <h1 ref={myRefs[index]}>{user.name}</h1>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Sub.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let num = 10;
+
+const Sub = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export {num};
+export default Sub;
